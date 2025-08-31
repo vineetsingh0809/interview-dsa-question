9f64f53ba9dec1f96b30bbda50e3ac16d2b3b906
vineetsingh0809 <ankitchilma@gmail.com>
2025-08-31 23:04:11 +0530

    added 2 more methods to reverse an string

--- string-questions/reverse_words.js
+++ string-questions/reverse_words.js
@@ -1,5 +1,23 @@
+// Method 1
+
 const reverseString = (givenString) => {
     return givenString.split("").reverse().join("");
 }
-
 console.log(reverseString("my name is ankit"))
+
+
+// Method 2
+
+const reverseWords = (str)=>{
+    return str.trim().split(/\s+/).reverse().join(" ")
+}
+console.log(reverseWords("the sky is blue"))
+console.log(reverseWords("  chandu ke chacha  "))
+
+
+// Method 3
+
+const reverseEachWord = (str) => {
+    return str.split(" ").map((item)=>item.split("").reverse().join("")).join(" ")
+}
+console.log(reverseEachWord("This is a program"))
